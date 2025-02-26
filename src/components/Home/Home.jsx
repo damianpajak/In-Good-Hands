ca0bb9d4a299c348cbb4ee6d9b53c28c08a51e67
Damian Pająk <damian.pajak01@gmail.com>
2025-02-26 17:25:27 +0100

    Add components to Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,1 +1,23 @@
-export const Home = () => {};
+import { HomeAboutMe } from "./HomeAboutMe";
+import { HomeContact } from "./HomeContact";
+import { HomeFooter } from "./HomeFooter";
+import { HomeHeader } from "./HomeHeader";
+import { HomeInfo } from "./HomeInfo";
+import { HomeInstruction } from "./HomeInstruction";
+import { HomeNavigation } from "./HomeNavigation";
+import { HomeWeHelp } from "./HomeWeHelp";
+
+export const Home = () => {
+  return (
+    <div>
+      <HomeNavigation />
+      <HomeHeader />
+      <HomeInfo />
+      <HomeInstruction />
+      <HomeAboutMe />
+      <HomeWeHelp />
+      <HomeContact />
+      <HomeFooter />
+    </div>
+  );
+};
