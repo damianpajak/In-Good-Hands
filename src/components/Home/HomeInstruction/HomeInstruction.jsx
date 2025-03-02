8b9a48637ef54db9a3728f4afbb645050c2dd3bc
Damian Pająk <damian.pajak01@gmail.com>
2025-03-02 20:12:37 +0100

    Update component

--- src/components/Home/HomeInstruction/HomeInstruction.jsx
+++ src/components/Home/HomeInstruction/HomeInstruction.jsx
@@ -1,10 +1,16 @@
+import { InstructionSteps } from "./InstructionSteps";
+
 export const HomeInstruction = () => {
   return (
     <div className="home-instruction">
-      <p>Wystarczą 4 proste kroki</p>
-      <p>zdjęcie</p>
-      <p>Instrukcja</p>
-      <p>Przycisk</p>
+      <span className="home-instruction__title">Wystarczą 4 proste kroki</span>
+      <img
+        className="home-instruction__image"
+        src="/Decoration.png"
+        alt="Dekoracja"
+      />
+      <InstructionSteps />
+      <span className="home-instruction__btn">ODDAJ RZECZY</span>
     </div>
   );
 };
