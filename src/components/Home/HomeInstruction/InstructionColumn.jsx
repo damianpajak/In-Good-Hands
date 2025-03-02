4bdd14d31aa9b824335e4e85d2a9742697c1db71
Damian Pająk <damian.pajak01@gmail.com>
2025-03-02 17:57:54 +0100

    Create column component for instruction component

---
+++ src/components/Home/HomeInstruction/InstructionColumn.jsx
@@ -0,0 +1,17 @@
+export const InstructionColumn = ({
+  num,
+  title,
+  txt,
+  style,
+  numStyle,
+  titleStyle,
+  txtStyle,
+}) => {
+  return (
+    <div className={style}>
+      <h1 className={numStyle}>{num}</h1>
+      <h2 className={titleStyle}>{title}</h2>
+      <h3 className={txtStyle}>{txt}</h3>
+    </div>
+  );
+};
