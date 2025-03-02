b28545f0d1f15424802344c5a72f56e6bd421357
Damian Pająk <damian.pajak01@gmail.com>
2025-03-02 17:55:23 +0100

    Add controling and strict mode for render element

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./scss/main.scss";
+import React from "react";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
 
-root.render(
-  <div>
-    <App />
-  </div>
-);
+if (!container) {
+  console.error("Błąd: Nie znaleziono elementu #root w index.html");
+} else {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
