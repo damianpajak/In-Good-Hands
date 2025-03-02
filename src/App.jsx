8a366d2aa257008a163a5c328baa1006f1fd2b92
Damian Pająk <damian.pajak01@gmail.com>
2025-03-02 17:54:31 +0100

    Update routing for home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 
 export const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
