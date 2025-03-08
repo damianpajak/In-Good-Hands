c44bfe4a43e407b26c72b1957f9968a83734324d
Damian Pająk <damian.pajak01@gmail.com>
2025-03-08 20:30:36 +0100

    Create component for footer

---
+++ src/components/Home/HomeContact/HomeContactFooter.jsx
@@ -0,0 +1,21 @@
+export const HomeContactFooter = () => {
+  return (
+    <div className="home-ContactFooter">
+      <div className="home-ContactFooter__copyrights">
+        Copyright by Coders Lab
+      </div>
+      <div className="home-ContactFooter__socialmedia">
+        <img
+          className="home-ContactFooter__icon"
+          src="/Facebook.png"
+          alt="facebook icon"
+        />
+        <img
+          className="home-ContactFooter__icon"
+          src="/Instagram.png"
+          alt="instagram icon"
+        />
+      </div>
+    </div>
+  );
+};
