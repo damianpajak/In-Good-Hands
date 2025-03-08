f5e860a375004e767ad3da10b1565b7884d6fac2
Damian Pająk <damian.pajak01@gmail.com>
2025-03-08 20:29:37 +0100

    Create component for home contact

--- src/components/Home/HomeContact/HomeContact.jsx
+++ src/components/Home/HomeContact/HomeContact.jsx
@@ -1,1 +1,15 @@
-export const HomeContact = () => {};
+import { HomeContactFooter } from "./HomeContactFooter";
+import { HomeContactForm } from "./HomeContactForm";
+
+export const HomeContact = () => {
+  return (
+    <div className="home-Contact">
+      <div className="home-Contact__Form">
+        <HomeContactForm />
+      </div>
+      <div className="home-Contact__Footer">
+        <HomeContactFooter />
+      </div>
+    </div>
+  );
+};
