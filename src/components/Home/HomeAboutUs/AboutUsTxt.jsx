1a91693b3d1056f60cd1d5d73f9c6deea2f971fb
Damian Pająk <damian.pajak01@gmail.com>
2025-03-08 11:18:51 +0100

    Create new components for about us page

---
+++ src/components/Home/HomeAboutUs/AboutUsTxt.jsx
@@ -0,0 +1,20 @@
+export const AboutUsTxt = () => {
+  return (
+    <div className="home-aboutUsTxt">
+      <h1 className="home-aboutUsTxt__title">O nas</h1>
+      <img
+        className="home-aboutUsTxt__image"
+        src="/Decoration.png"
+        alt="decoration"
+      />
+      <p className="home-aboutUsTxt__description">
+        Nori grape silver beet broccoli kombu beet greens fava bean potato
+        quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil
+        turnip greens parsnip.
+      </p>
+      <div className="home-aboutUsTxt__signature">
+        <img src="/Signature.png" alt="podpis" />
+      </div>
+    </div>
+  );
+};
